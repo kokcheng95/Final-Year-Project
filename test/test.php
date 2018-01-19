d97515ff4dc80d43e87e4059259a50c68898b3ab
chengjunyong <junyong1213@hotmail.com>
2018-01-20 01:07:13 +0800

    Update test.php

--- test/test.php
+++ test/test.php
@@ -1,51 +1,21 @@
 <!DOCTYPE html>
-
 <html>
-
 <head>
-    
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>freequeue</title>
-    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Cookie">
-    <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
-    <link rel="stylesheet" href="assets/fonts/material-icons.css">
-    <link rel="stylesheet" href="assets/css/Pretty-Footer.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.10.0/css/lightbox.min.css">
-    <link rel="stylesheet" href="assets/css/Pretty-Registration-Form.css">
-    <link rel="stylesheet" href="assets/css/styles.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <script>
-        $(document).ready(function(){
-
-            $("#btn").click(function(){
-                 
-              $.ajax({
-                
-                    type: "POST",
-                    url: "testdb.php",
-                    data: {
-                          username:$("#username").val(),
-                          email:$("#em").val(),
-                          password:$("#ps").val()
-
-                    },
-                     
-                     success: function(){
-
-                    },
-                    
-                   
-                        });
-
-
-                });
-
-         });</script>
-   
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>freequeue</title>
+<link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Cookie">
+<link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
+<link rel="stylesheet" href="assets/fonts/material-icons.css">
+<link rel="stylesheet" href="assets/css/Pretty-Footer.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.10.0/css/lightbox.min.css">
+<link rel="stylesheet" href="assets/css/Pretty-Registration-Form.css">
+<link rel="stylesheet" href="assets/css/styles.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src="assets/bootstrap/js/bootstrap.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.10.0/js/lightbox-plus-jquery.min.js"></script>  
 </head>
-
 <body>
     <nav class="navbar navbar-inverse navbar-fixed-top">
         <div class="container">
@@ -62,58 +32,66 @@
             </div>
         </div>
     </nav>
-   
-            <div class="row register-form">
-                <div class="col-md-8 col-md-offset-2">
-                    <form class="form-horizontal custom-form">
-                        <h1>Register Form</h1>
-                        <div class="form-group" >
-                            <div class="col-sm-4 label-column " >
-                                <label class="control-label" for="name-input-field">Name </label>
-                            </div>
-                            <div class="col-sm-6 input-column validate-input" data-validate = "Valid Name is required">
-                                <input class="form-control " required  type="text" id="username">
-                            </div>
+    <div class="row register-form">
+        <div class="col-md-8 col-md-offset-2">
+            <form class="form-horizontal custom-form" method="post">
+                <h1>Register Form</h1>
+                    <div class="form-group" >
+                        <div class="col-sm-4 label-column">
+                            <label class="control-label">Name </label>
                         </div>
-                        <div class="form-group">
-                            <div class="col-sm-4 label-column">
-                                <label class="control-label" for="email-input-field">Email </label>
-                            </div>
-                            <div class="col-sm-6 input-column">
-                                <input class="form-control" required type="email" id="em">
-                            </div>
+                        <div class="col-sm-6 input-column validate-input">
+                                <input class="form-control" type="text" id="username" required>
                         </div>
-                        <div class="form-group">
-                            <div class="col-sm-4 label-column" data-validate = "password Name is required">
-                                <label class="control-label" for="pawssword-input-field">Password </label>
-                            </div>
-                            <div class="col-sm-6 input-column">
-                                <input class="form-control" required  type="password" id="ps">
-                            </div>
+                    </div>
+                    <div class="form-group">
+                        <div class="col-sm-4 label-column">
+                             <label class="control-label">Email </label>
+                         </div>
+                         <div class="col-sm-6 input-column">
+                            <input class="form-control" type="email" id="email" required>
+                         </div>
+                    </div>
+                    <div class="form-group">
+                        <div class="col-sm-4 label-column">
+                            <label class="control-label">Password </label>
                         </div>
-                        <div class="form-group">
-                            <div class="col-sm-4 label-column">
-                                <label class="control-label" for="repeat-pawssword-input-field">Repeat Password </label>
-                            </div>
-                            <div class="col-sm-6 input-column">
-                                <input class="form-control" required id="repeat-pawssword-input-field" type="password" title="Please use only 6 digit">
-                            </div>
+                        <div class="col-sm-6 input-column">
+                            <input class="form-control" type="password" id="password1" required>
                         </div>
+                    </div>
+                    <div class="form-group">
+                        <div class="col-sm-4 label-column">
+                            <label class="control-label">Repeat Password </label>
+                        </div>
+                        <div class="col-sm-6 input-column">
+                            <input class="form-control" type="password" id="password2" required>
+                        </div>
+                    </div>
+                    <div class="checkbox">
+                        <label><input type="checkbox" required>I've read and accept the terms and conditions</label>
+                    </div>
+                    <div>
+                        <button class="btn btn-default submit-button" type="submit-button" id="btn_register">Register</button>
+                    </div>
+            </form
 
-                        <div class="checkbox">
-                            <label>
-                                <input type="checkbox" required>I've read and accept the terms and conditions</label>
-                        </div>
-                        <div>
-                        <button class="btn btn-default submit-button" type="submit-button" id="btn">Register</button></div>
-                    </form>
-                </div>
-            </div>
-    <script src="assets/js/jquery.min.js"></script>
-    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.10.0/js/lightbox-plus-jquery.min.js"></script>
-    
-    
+            <button class="btn btn_primary" id="btn">test</button>
+        </div>
+    </div>
+<script>
+$(document).ready(function(){
+    $("#btn_register").click(function(){
+        var a = document.getElementById('password1');
+        var b = document.getElementById('password2');
+        if(a.value != b.value){
+            b.setCustomValidity('Password must be same as above');
+        }else{
+            b.setCustomValidity('');
+            $.ajax({type: "POST", url: "testdb.php",data: {username:$("#username").val(),email:$("#em").val(),password:$("#ps").val()},success: function(){},});
+            }
+    });
+});                                                              
+</script>   
 </body>
-
 </html>
